857bab8c2ee7f3d21f0466bb36f41ba360c064fc
Paweł Kruszko <pawelkruszko@gmail.com>
2023-12-05 18:56:55 +0100

    unit test improve

--- lists/views.py
+++ lists/views.py
@@ -6,12 +6,13 @@ def home_page(request):
     return render(request, "home.html")
 
 
-def view_list(request):
-    items = Item.objects.all()
-    return render(request, "list.html", {"items": items})
-
-
 def new_list(request):
     nulist = List.objects.create()
     Item.objects.create(text=request.POST["item_text"], list=nulist)
-    return redirect("/lists/the-only-list-in-the-world/")
+    return redirect(f"/lists/{nulist.id}/")
+
+
+def view_list(request, list_id):
+    our_list = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=our_list)
+    return render(request, "list.html", {"items": items})
